a09418c51c1063fc564d23d110cea51d6e9bf3f4
on1ystar <tjdwls0607@naver.com>
2022-12-20 14:28:34 +0000

    develop MemberService

---
+++ practice/src/main/java/com/example/practice/service/MemberService.java
@@ -0,0 +1,51 @@
+package com.example.practice.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.practice.domain.Member;
+import com.example.practice.repository.MemberRepository;
+import com.example.practice.repository.MemoryMemberRepository;
+
+public class MemberService {
+    
+    // private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // DI를 위한 생성자
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+    
+    
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 한 명 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
