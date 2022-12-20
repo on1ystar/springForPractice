b9ae5b95c0b9a5f7ff356458aa24038e058fc4c0
on1ystar <tjdwls0607@naver.com>
2022-12-20 14:16:19 +0000

    develop MemoryMemberRepositoryTest class & success to testing

--- practice/src/main/java/com/example/practice/repository/MemoryMemberRepository.java
+++ practice/src/main/java/com/example/practice/repository/MemoryMemberRepository.java
@@ -34,5 +34,9 @@ public class MemoryMemberRepository implements MemberRepository{
         return new ArrayList<>(store.values());
     }
 
+    public void clearStore() {
+        store.clear();
+    }
+
     
 }
