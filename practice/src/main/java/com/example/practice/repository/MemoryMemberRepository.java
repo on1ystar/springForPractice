f8404e70a0a72dfc291f9174a4cd77e015bed551
on1ystar <tjdwls0607@naver.com>
2022-12-28 07:13:40 +0000

    MemoryMemberRepository 수정하기

--- practice/src/main/java/com/example/practice/repository/MemoryMemberRepository.java
+++ practice/src/main/java/com/example/practice/repository/MemoryMemberRepository.java
@@ -9,7 +9,6 @@ public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
-
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
@@ -37,6 +36,4 @@ public class MemoryMemberRepository implements MemberRepository{
     public void clearStore() {
         store.clear();
     }
-
-    
 }
